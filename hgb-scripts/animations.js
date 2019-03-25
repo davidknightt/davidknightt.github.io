e41f07e1e0d7b0510ca5cfb2ed6214198c44d59a
David Knight <david.knight@hellogbye.com>
2019-03-25 10:22:43 -0400

    DK - entire tab section now changes on user swipe

--- hgb-scripts/animations.js
+++ hgb-scripts/animations.js
@@ -280,6 +280,72 @@ function handleTabMouseUp(event) {
   window.USER_IS_TOUCHING = false;
 }
 
+//Swipe effect: http://gilmation.com/articles/javascript-event-delegation-for-a-horizontal-swipe-effect/
+var touchStartCoords =  {'x':-1, 'y':-1}, // X and Y coordinates on mousedown or touchstart events.
+    touchEndCoords = {'x':-1, 'y':-1},// X and Y coordinates on mouseup or touchend events.
+    direction = 'undefined',// Swipe direction
+    minDistanceXAxis = 30,// Min distance on mousemove or touchmove on the X axis
+    maxDistanceYAxis = 30,// Max distance on mousemove or touchmove on the Y axis
+    maxAllowedTime = 1000,// Max allowed time between swipeStart and swipeEnd
+    startTime = 0,// Time on swipeStart
+    elapsedTime = 0,// Elapsed time between swipeStart and swipeEnd
+    targetElement = document.getElementById('tabs-section');// Element to delegate
+
+function swipeStart(e) {
+  e = e ? e : window.event;
+  e = ('changedTouches' in e)?e.changedTouches[0] : e;
+  touchStartCoords = {'x':e.pageX, 'y':e.pageY};
+  startTime = new Date().getTime();
+  //targetElement.textContent = " ";
+}
+
+function swipeMove(e){
+  e = e ? e : window.event;
+  e.preventDefault();
+}
+
+function swipeEnd(e) {
+  e = e ? e : window.event;
+  e = ('changedTouches' in e)?e.changedTouches[0] : e;
+  touchEndCoords = {'x':e.pageX - touchStartCoords.x, 'y':e.pageY - touchStartCoords.y};
+  elapsedTime = new Date().getTime() - startTime;
+  if (elapsedTime <= maxAllowedTime){
+    if (Math.abs(touchEndCoords.x) >= minDistanceXAxis && Math.abs(touchEndCoords.y) <= maxDistanceYAxis){
+      direction = (touchEndCoords.x < 0)? 'left' : 'right';
+      switch(direction){
+        case 'left':
+          eventFire(document.getElementById('Tab 2'), 'click');
+          break;
+        case 'right':
+          eventFire(document.getElementById('Tab 1'), 'click');
+          break;
+      }
+    }
+  }
+}
+
+// Simulates a 'click' when called
+function eventFire(el, etype){
+  if (el.fireEvent) {
+    el.fireEvent('on' + etype);
+  } else {
+    var evObj = document.createEvent('Events');
+    evObj.initEvent(etype, true, false);
+    el.dispatchEvent(evObj);
+  }
+}
+
+function addMultipleListeners(el, s, fn) {
+  var evts = s.split(' ');
+  for (var i=0, iLen=evts.length; i<iLen; i++) {
+    el.addEventListener(evts[i], fn, false);
+  }
+}
+
+addMultipleListeners(targetElement, 'mousedown touchstart', swipeStart);
+addMultipleListeners(targetElement, 'mousemove touchmove', swipeMove);
+addMultipleListeners(targetElement, 'mouseup touchend', swipeEnd);
+
 function animateCircuitWires() {
   setTimeout(function(){
     $('.banner-image').addClass('active');
